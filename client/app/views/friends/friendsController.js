be70eafbf81abb0a9f0a71924a0f05e7b1cd06a5
lars-uninett <larssth@uninett.no>
2014-07-24 15:36:06 +0200

    Minor UI updates

--- client/app/views/friends/friendsController.js
+++ client/app/views/friends/friendsController.js
@@ -14,6 +14,14 @@ app.controller( 'friendsController', function(main, $state, $scope, model, fileD
 		return Object.keys(friends).length - 1;
 	};
 	
+	/**
+	 * Get number of groups
+	 * @returns {Number}
+	 */
+	$scope.getNumGroups = function() {
+		return Object.keys($scope.groups.list).length;
+	};
+	
 	$scope.actionCallSelected = function(friend) {
 		console.log(friend);
 	};
