c478659bebe4bcf099d689c84dd00f794bcc0d93
Simen Owesen-Lein <simen.owesenlein@gmail.com>
2014-07-29 17:39:46 +0200

    removed things, still bugs

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,6 @@
 var app = angular.module( 'app', [
 	'ui.router',
 	'ngAnimate',
-	'mainModule',
 	'ng-context-menu',
 	'dialogs.main'
 ]);
@@ -9,10 +8,47 @@ app.config( function ( $stateProvider) {
 	$stateProvider.state('base', {
 		controller: "mainController",
 		templateUrl: "app/views/main/main.tpl.html"
+	}).state('standard', {
+		parent: 'base',
+		views: {
+			"view1": {
+				controller: "chatHistoryController",
+				templateUrl: "app/views/chatHistory/chatHistory.tpl.html"
+			},
+			"view3": {
+				controller: "friendsController",
+				templateUrl: "app/views/friends/friends.tpl.html"
+			},
+		}
+	}).state('chat', {
+		parent: 'standard',
+		views: {
+			"view2@base": {
+				controller: "chatController",
+				templateUrl: "app/views/chat/chat.tpl.html"
+			},
+		}
+	}).state('conference', {
+		parent: 'standard',
+		views: {
+
+		}
+	}).state('conference.fullscreen', {
+		parent: 'standard',
+		views: {
+
+		}
+	}).state('call', {
+		parent: "base",
+		views: {
+			"view2@base": {
+				controller: "videoActiveController",
+				templateUrl: "app/views/video/videoActive.tpl.html"
+			},
+		}
 	});
 });
 
 app.run( function (main) {
 	main.init();
 });
-
