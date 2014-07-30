855726bffb370b44d4cd3641dba356b861f2dee0
Simen Owesen-Lein <=>
2014-07-30 20:56:03 +0200

    hlkh

--- client/app/services/xmpp/presence.js
+++ client/app/services/xmpp/presence.js
@@ -1,6 +1,6 @@
 (function () {
 
-	app.factory('presence', function(xmpp,  constants, $rootScope, utility) {
+	app.factory('presence', function(xmpp,  constants, $rootScope, utility, chat) {
 		var presence = {
 			init: function() {
 				xmpp.addHandler(xmppHandlers.mucPresence, constants.xmpp.mucUser, "presence");
