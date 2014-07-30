e8cb9326e874fdbe98346964b0ff16d1b70d2781
lars-uninett <larssth@uninett.no>
2014-07-30 10:22:25 +0200

    Added reset button to search

--- client/app/views/friends/friendsController.js
+++ client/app/views/friends/friendsController.js
@@ -100,6 +100,11 @@ app.controller( 'friendsController', function(main, $state, $scope, model, fileD
 		});
 	};
 	
+	$scope.resetSearch = function() {
+		$scope.setLocalSearch();
+		$scope.search.query = '';
+	};
+	
 	/**
 	 * Check if we're currently filtering the friends list
 	 * @returns {Boolean}
