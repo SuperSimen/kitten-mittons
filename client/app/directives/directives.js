3df92f9122507499448a46b69913d4e2b9bc08b6
lars-uninett <larssth@uninett.no>
2014-08-01 10:09:46 +0200

    Fixed full-screen video scaling

--- client/app/directives/directives.js
+++ client/app/directives/directives.js
@@ -1,5 +1,37 @@
 (function () {
     
+	/**
+	 * Maximize video scale while keeping the whole element visible on screen
+	 */
+	app.directive("ngVideoResize", function() {
+		
+		var e = null;
+		
+		function resize() {
+			
+			var max_scale = Math.min(
+				window.innerWidth / e.videoWidth, 
+				window.innerHeight / e.videoHeight
+			);
+
+			e.style.width = e.width = max_scale * e.videoWidth;
+			e.style.width = e.height = max_scale * e.videoHeight;
+		}
+		
+		return {
+			compile: function(element) {
+				e = element[0];
+				window.addEventListener('resize', resize);
+				e.addEventListener('loadedmetadata', resize);
+			},
+			link: function(scope, element) {
+				scope.on('$destroy', function() {
+					element[0].removeEventListener('resize', resize);
+				});
+			}
+		};
+	});
+	
 	app.directive("ngDropFile", function() {
 		return function(scope, element, attrs) {
 			
